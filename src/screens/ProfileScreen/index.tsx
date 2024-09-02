2d821488e295cb487f51ae752bc08c829866c277
Naeemul_Haq <naeemcharbagh1274@gmail.com>
2024-09-02 17:43:36 +0500

    changes

--- src/screens/ProfileScreen/index.tsx
+++ src/screens/ProfileScreen/index.tsx
@@ -4,8 +4,19 @@ import { ScrollView, ScrollViewBase, StyleSheet, Text, View } from 'react-native
 import React from 'react'
 import { MyButton, MyText, MyTextInput } from '@components'
 import { MY_COLORS } from '@constants'
+import { useAuth } from '@context'
+import Clipboard from '@react-native-clipboard/clipboard';
+import { showToast } from '@utils'
 
-const index = () => {
+
+const ProfileScreen = () => {
+const {currentUser} = useAuth()
+
+const handleCopyPress = () => {
+  Clipboard.setString(currentUser?.uid||""); // Copies the text to the clipboard
+  showToast('Success', 'Text copied to clipboard!'); // Shows an alert to indicate success
+};
+
   return (
     <View style={{ backgroundColor: 'black', height: '100%', paddingVertical: 20, paddingHorizontal: 12 }}>
       <ScrollView>
@@ -19,8 +30,8 @@ const index = () => {
         <View>
           <MyText color='white'>My Member ID</MyText>
           <View style={{ gap: 10, flexDirection: 'row', alignItems: 'center' }}>
-            <MyText color='white'>12132123453453</MyText>
-            <MyButton title='Copy' onPress={() => { }} style={{ height: 25, width: 50 }} />
+            <MyText color='white'>{currentUser?.uid}</MyText>
+            <MyButton title='Copy' onPress={handleCopyPress} style={{ height: 25, width: 50 }} />
           </View>
         </View>
         <View style={{ gap: 10 }}>
@@ -41,26 +52,12 @@ const index = () => {
               <MyButton title='Reject' onPress={() => { }} style={{ flex: 1 }} />
             </View>
           </View>
-          <View style={{ gap: 10, backgroundColor: MY_COLORS.PRIMARY, justifyContent: 'center', padding: 10, borderRadius: 10 }}>
-            <MyText color='white' style={{ textAlign: 'center' }}>Ahmad Invite you</MyText>
-            <View style={{ gap: 10, flexDirection: 'row', justifyContent: 'space-between' }}>
-              <MyButton title='Accept' onPress={() => { }} style={{ flex: 1 }} />
-              <MyButton title='Reject' onPress={() => { }} style={{ flex: 1 }} />
-            </View>
-          </View>
-          <View style={{ gap: 10, backgroundColor: MY_COLORS.PRIMARY, justifyContent: 'center', padding: 10, borderRadius: 10 }}>
-            <MyText color='white' style={{ textAlign: 'center' }}>Ahmad Invite you</MyText>
-            <View style={{ gap: 10, flexDirection: 'row', justifyContent: 'space-between' }}>
-              <MyButton title='Accept' onPress={() => { }} style={{ flex: 1 }} />
-              <MyButton title='Reject' onPress={() => { }} style={{ flex: 1 }} />
-            </View>
-          </View>
         </View>
       </ScrollView>
     </View>
   )
 }
 
-export default index
+export default ProfileScreen
 
 const styles = StyleSheet.create({})
