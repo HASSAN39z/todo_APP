57a0f01001cb59000701b9a60a2a8f4deb9a2b9a
Naeemul_Haq <naeemcharbagh1274@gmail.com>
2024-09-02 12:33:47 +0500

    commit

--- ios/ToDoListTests/ToDoListTests.m
+++ ios/ToDoListTests/ToDoListTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface todoAppTests : XCTestCase
+@interface ToDoListTests : XCTestCase
 
 @end
 
-@implementation todoAppTests
+@implementation ToDoListTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
--- src/navigation/AuthNav/index.tsx
+++ src/navigation/AuthNav/index.tsx
@@ -7,13 +7,13 @@ import { LoginScreen } from '@screens';
 const App = () => {
   const { currentUser, initializing, loading } = useAuth();
 
-  // if (initializing || loading) return <LoadingScreen description='Login please wait...' />
+  if (initializing || loading) return <LoadingScreen description='Login please wait...' />
   // if (currentUser) return <BottomTabNavigation />
   // return <LoginScreen />
 
   return (
     <>
-      {false ? <BottomTabNavigation /> : <LoginScreen />}
+      {currentUser ? <BottomTabNavigation /> : <LoginScreen />}
     </>
   );
 };
